b9816e2319a5a1768055a5545c97c496338d5f09
valzate <valzate223@gmail.com>
2026-02-05 19:09:41 -0500

    ya se resolvio el problema 1

--- problema1/main.go
+++ problema1/main.go
@@ -11,28 +11,30 @@ import (
 
 func worker(id int, veces int, wg *sync.WaitGroup) {
 	// TODO: asegurar que al finalizar la función se haga wg.Done()
-	
 
 	for i := 1; i <= veces; i++ {
 		fmt.Printf("[worker %d] hola %d\n", id, i)
 		// TODO: dormir un poco para simular trabajo (p. ej. 100–300 ms)
-		
+		time.Sleep(time.Duration(100+id*50) * time.Millisecond)
 	}
+	wg.Done()
 }
 
 func main() {
 	var wg sync.WaitGroup
 
 	// TODO: cambiar estos parámetros y observar el intercalado de salidas
-	// numGoroutines
-	// veces
+	numGoroutines := 4
+	veces := 1
 
 	// TODO: lanzar varias goroutines, sumar al WG y esperar con wg.Wait()
 	for id := 1; id <= numGoroutines; id++ {
-
+		wg.Add(1)
+		go worker(id, veces, &wg)
 	}
 
 	// Esperar a que todas las goroutines terminen
-	
+
+	wg.Wait()
 	fmt.Println("Listo: todas las goroutines terminaron.")
 }
