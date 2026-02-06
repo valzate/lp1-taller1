65ada941a8733afd742eb9224fc0f668016df1fa
valzate <valzate223@gmail.com>
2026-02-05 19:20:17 -0500

    ya se resolvio el problema 2

--- problema2/main.go
+++ problema2/main.go
@@ -20,7 +20,7 @@ func secuencial(durs []time.Duration) time.Duration {
 	inicio := time.Now()
 	// TODO: ejecutar las tareas en orden, sin goroutines
 	for i, d := range durs {
-
+		tarea(i+1, d)
 	}
 	return time.Since(inicio)
 }
@@ -30,7 +30,11 @@ func concurrente(durs []time.Duration) time.Duration {
 	var wg sync.WaitGroup
 	// TODO: lanzar cada tarea en su propia goroutine y esperar con WaitGroup
 	for i, d := range durs {
-
+		wg.Add(1)
+		go func(id int, dur time.Duration) {
+			defer wg.Done()
+			tarea(id, dur)
+		}(i+1, d)
 	}
 	wg.Wait()
 	return time.Since(inicio)
@@ -38,12 +42,12 @@ func concurrente(durs []time.Duration) time.Duration {
 
 func main() {
 	// TODO: experimenta con diferentes duraciones
-	// durs := []time.Duration{700 * time.Millisecond, 500 * time.Millisecond, 1 * time.Second}
+	durs := []time.Duration{800 * time.Millisecond, 600 * time.Millisecond, 2 * time.Second}
 
-	// d1 := 
+	d1 := secuencial(durs)
 	fmt.Println("Duración SEC:", d1)
 
-	// d2 := 
+	d2 := concurrente(durs)
 	fmt.Println("Duración CONC:", d2)
 
 	fmt.Println("Nota: la ejecución concurrente debería ser ~max(durs). Cambia valores y observa.")
