f1d84bcd9f8224db962a16edfdde392ce4db4016
valzate <valzate223@gmail.com>
2026-02-09 20:00:49 -0500

    ya se resolvio el problema 8

--- problema8/main.go
+++ problema8/main.go
@@ -14,20 +14,38 @@ func asyncCuadrado(x int) <-chan int {
 	go func() {
 		defer close(ch)
 		// TODO: simular trabajo
-
+		time.Sleep(time.Millisecond * 100)
 		ch <- x * x
 	}()
 	return ch
 }
 
+// Pista: crea una función fanIn que recibe múltiples <-chan int y retorna un único <-chan int
+// que emita todos los valores. Requiere goroutines y cerrar el canal de salida cuando todas terminen
+func fanIn(channels ...<-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, ch := range channels {
+			for val := range ch {
+				out <- val
+			}
+		}
+	}()
+	return out
+}
+
 func main() {
 	// TODO: crea varios futuros y recolecta sus resultados: f1, f2, f3
+	f1 := asyncCuadrado(8)
+	f2 := asyncCuadrado(14)
+	f3 := asyncCuadrado(42)
 
 	// TODO: Opción 1: esperar cada futuro secuencialmente
 
-	
 	// TODO: Opción 2: fan-in (combinar múltiples canales)
-	// Pista: crea una función fanIn que recibe múltiples <-chan int y retorna un único <-chan int
-	// que emita todos los valores. Requiere goroutines y cerrar el canal de salida cuando todas terminen.
-	
+	merged := fanIn(f1, f2, f3)
+	for result := range merged {
+		fmt.Println(result)
+	}
 }
