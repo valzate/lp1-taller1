878381c5fdb8e771313e44404866d4e04fa5eca3
valzate <valzate223@gmail.com>
2026-02-09 19:46:14 -0500

    ya se resolvio el problema 9

--- problema9/main.go
+++ problema9/main.go
@@ -15,21 +15,23 @@ import (
 type tenedor struct{ mu sync.Mutex }
 
 func filosofo(id int, izq, der *tenedor, wg *sync.WaitGroup) {
-	// TODO: desarrolla el código para el filósofo
-	
+	defer wg.Done()
+	// TODO: desarrolla el código para el filósofo que piensa, toma tenedores en orden seguro, come y luego suelta los tenedores.
+
 	fmt.Printf("[filósofo %d] satisfecho\n", id)
 }
 
 func pensar(id int) {
 	fmt.Printf("[filósofo %d] pensando...\n", id)
-	// TODO: simular tiempo de pensar
 
+	// TODO: simular tiempo de pensar
+	time.Sleep(time.Duration(1+id) * time.Second) // ejemplo: cada filósofo piensa un tiempo distinto
 }
 
 func comer(id int) {
 	fmt.Printf("[filósofo %d] COMIENDO\n", id)
 	// TODO: simular tiempo de pensar
-
+	time.Sleep(time.Duration(1+id) * time.Second) // ejemplo: cada filósofo come un tiempo distinto
 }
 
 func main() {
@@ -41,16 +43,18 @@ func main() {
 	forks := make([]*tenedor, n)
 	for i := 0; i < n; i++ {
 		// TODO: inicializar cada tenedor i
+		forks[i] = &tenedor{}
+		var izq, der *tenedor
+		if i < n-1 {
+			izq, der = forks[i], forks[i+1]
+		} else {
+			izq, der = forks[i], forks[0]
+		}
+		go filosofo(i, izq, der, &wg)
 
 	}
 
-	// lanzar filósofos
-	for i := 0; i < n; i++ {
-		izq := forks[i]
-		der := forks[(i+1)%n]
-		// TODO: lanzar goroutine para el filósofo i
-
-	}
+	// lanzar filósofos (ya están lanzadas en el loop anterior)
 
 	wg.Wait()
 	fmt.Println("Todos los filósofos han comido sin deadlock.")
